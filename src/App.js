2d70300b604560187dc98ee3d998c1a89dc88b0d
PedroArchangelo123 <pedroarchangelopedro@gmail.com>
2025-04-28 19:34:05 -0300

    Last commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap'; // ✅ Correto agora
+import Formulario from './Components/Formulario/index';
+import Lista from './Components/ListProdutos/index';
+import Header from './Components/Header';
+
+
+
+
+function App() {
+  const [selectedProduto, setSelectedProduto] = useState(null);
+  const [RefreshLista, setRefreshLista] = useState(false);
+
+  const handleEdit = (produto) => {
+    setSelectedProduto(produto);
+  };
+
+  const handleSave = () => {
+    setSelectedProduto(null);
+    setRefreshLista(!RefreshLista);
+  };
+
+  return (
+    <Container className="mt-5"> {/* ✅ Corrigido aqui */}
+       <Header/>
+      <h1>Cadastro de Produtos</h1>
+      <Formulario selectedProduto={selectedProduto} onSave={handleSave} />
+      <hr />
+      <Lista onEdit={handleEdit} RefreshLista={RefreshLista} />
+  
+    </Container>    
+
+  )
+}
+
+export default App;
